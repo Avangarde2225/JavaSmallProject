8c8797916258c67a1ed1545e46829e5a088b8245
TechnoStudy <info@techno.study>
2020-03-10 19:47:31 -0400

    coreJava

--- src/test/java/coreJava/mapRecap.java
+++ src/test/java/coreJava/mapRecap.java
@@ -1,5 +1,6 @@
 package coreJava;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class mapRecap {
@@ -43,7 +44,7 @@ public class mapRecap {
             System.out.println(s1);
         }
 
-    }
+
 
 /*
 
@@ -55,7 +56,108 @@ HomeWork 2:  Most frequent characters
             answer should be d
                     and count = 4
 
- */
+*/
 
+//  Homework 1
+
+    ArrayList<String> s1 = new ArrayList<String>(  );
+
+    s1.add( "Apple" );
+    s1.add( "Apple" );
+    s1.add( "Banana" );
+    s1.add( "Banana" );
+    s1.add( "Orange" );
+
+
+
+    String result12 = null;
+
+    outerloop:
+    for(int i=0; i<s1.size() ; i++ ){
+
+        for(int j = 0; j<s1.size() ; j++){
+
+            String i1 = s1.get( i );
+            String j1 = s1.get( j );
+
+            int size = s1.size()-1;
+
+            System.out.println(i1 + " "+ j + " " + result12 + " --" + j1 );
+
+            if(i == size && !i1.isEmpty()){
+                result12 =s1.get( i );
+                break;
+            }else if( i==j ){
+                continue;
+            }else if(i1.equals( j1 )){
+                continue outerloop;
+            }else if(!i1.equals( j1) && j ==size){
+                result12 = s1.get( i );
+                break outerloop;
+            }
+
+        }
+
+    }
+
+        System.out.println("result == "+result12);
+
+//   Homework 2
+
+//        solution one
+        String a = "aasssddddgggggg";
+
+        char c1 = 0;
+        int count = 0 ;
+        int max = 0 ;
+
+        for(int i = 0 ; i < a.length() ; i++){
+            count = 0 ;
+            for(int j = 0 ; j<a.length() ;j++){
+
+                if(a.charAt( i ) == a.charAt( j )){
+                    count++;
+                }
+
+                if(count > max ){
+                    max = count;
+                    c1 = a.charAt( i );
+                }
+
+            }
+
+            System.out.println(c1);
+            System.out.println(max);
+        }
+
+
+//        -------------------------
+
+        String ab = "aasssddddgggggg";
+        char[] ar=ab.toCharArray();
+        int countA=0;
+        int countS=0;
+        int countD=0;
+
+        for (int i = 0; i <ar.length ; i++) {
+            if(ar[i]=='a'){
+                countA++;
+            }else if(ar[i]=='s'){
+                countS++;
+            }else if(ar[i]=='d'){
+                countD++;
+            }
+        }
+
+        if(countA>countD && countA>countS){
+            System.out.println("a = "+countA);
+        }else if(countD>countA && countD>countS){
+            System.out.println("d = "+countD);
+        }else if (countS>countD && countS>countA){
+            System.out.println("s = "+countS);
+        }
+
+
+    }
 
 }
