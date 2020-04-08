dfa76d38d3487650e8544c170b5a8a1456ee6fcd
Serdar <serdar2225@gmail.com>
2020-04-08 13:01:19 -0400

    Math Equation calculation

---
+++ src/test/java/coreJava/loops.java
@@ -0,0 +1,21 @@
+package coreJava;
+
+import java.util.Scanner;
+
+
+public class loops {
+
+    public static void main(String[] args) {
+    }
+
+    public static void printStars(int number) {
+        // first part of the exercise
+        for (int n = 0; n < number; n++) {
+            System.out.print("*");
+
+        }
+        System.out.println("");
+
+
+    }
+}
