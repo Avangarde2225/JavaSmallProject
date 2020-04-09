534423ccffb299d2de521045bb23d8dae224d72e
Serdar <serdar2225@gmail.com>
2020-04-09 10:47:08 -0400

    reformatted

--- src/test/java/FlightRelated/Flight.java
+++ src/test/java/FlightRelated/Flight.java
@@ -3,7 +3,7 @@ package FlightRelated;
 public class Flight {
     private int passengers, flightNumber, seats = 150;
     private char flightClass;
-    private boolean [] isSeatAvailable;
+    private boolean[] isSeatAvailable;
     private int totalCheckedBags;
     int maxCarryOns = seats * 2, totalCarryOns;
 
@@ -14,31 +14,37 @@ public class Flight {
             isSeatAvailable[i] = true;
         }
     }
-        public void add1Passenger(){
-            if(hasSeating()){
-                passengers +=1;
-            } else{
-                handleTooMany();
-            }
+
+    public void add1Passenger() {
+        if (hasSeating()) {
+            passengers += 1;
+        } else {
+            handleTooMany();
         }
+    }
 
     private void handleTooMany() {
     }
 
-    private boolean hasSeating(){
-        return passengers<seats;
-        }
-        private boolean hasCarryOnSpace(int carryOns){
-        return totalCarryOns + carryOns <= maxCarryOns;
-        }
-
-        public Flight(){}
-        public Flight(int flightNumber){
-        this.flightNumber = flightNumber;
-        }
-        public Flight(char flightClass){
-        this.flightClass = flightClass;
-        }
-
+    private boolean hasSeating() {
+        return passengers < seats;
     }
+
+    private boolean hasCarryOnSpace(int carryOns) {
+
+        return totalCarryOns + carryOns <= maxCarryOns;
+    }
+
+    public Flight() {
+    }
+
+    public Flight(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public Flight(char flightClass) {
+        this.flightClass = flightClass;
+    }
+
+}
 
