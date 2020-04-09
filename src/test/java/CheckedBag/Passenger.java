f2364df1374fdaf6335706d1677867898ead1d33
Serdar <serdar2225@gmail.com>
2020-04-09 09:57:46 -0400

    Math Equation calculation

---
+++ src/test/java/CheckedBag/Passenger.java
@@ -0,0 +1,34 @@
+package CheckedBag;
+
+public class Passenger {
+    private double bagPerFee;
+    private int checkedBags;
+    private int freeBags;
+
+    public Passenger(){
+
+    }
+    public Passenger(int freeBags){
+        this(freeBags > 1 ? 25.0d : 50.0d);
+        this.freeBags = freeBags;
+    }
+    public Passenger(int freeBags, int checkedBags){
+        this(freeBags);
+        this.checkedBags = checkedBags;
+
+    }
+    private Passenger(double bagPerFee){
+        this.bagPerFee = bagPerFee;
+    }
+
+    public static void main(String[] args) {
+        Passenger fred = new Passenger(2);
+        System.out.println(fred.bagPerFee);
+
+        Passenger john = new Passenger(2,3);
+        System.out.println(john.bagPerFee);
+    }
+
+
+
+}
