65f9d5423042a6579ec91506830435c5edef97b1
Serdar <serdar2225@gmail.com>
2020-04-09 10:45:28 -0400

    Math Equation calculation

---
+++ src/test/java/FlightRelated/Flight.java
@@ -0,0 +1,44 @@
+package FlightRelated;
+
+public class Flight {
+    private int passengers, flightNumber, seats = 150;
+    private char flightClass;
+    private boolean [] isSeatAvailable;
+    private int totalCheckedBags;
+    int maxCarryOns = seats * 2, totalCarryOns;
+
+
+    {
+        isSeatAvailable = new boolean[seats];
+        for (int i = 0; i < seats; i++) {
+            isSeatAvailable[i] = true;
+        }
+    }
+        public void add1Passenger(){
+            if(hasSeating()){
+                passengers +=1;
+            } else{
+                handleTooMany();
+            }
+        }
+
+    private void handleTooMany() {
+    }
+
+    private boolean hasSeating(){
+        return passengers<seats;
+        }
+        private boolean hasCarryOnSpace(int carryOns){
+        return totalCarryOns + carryOns <= maxCarryOns;
+        }
+
+        public Flight(){}
+        public Flight(int flightNumber){
+        this.flightNumber = flightNumber;
+        }
+        public Flight(char flightClass){
+        this.flightClass = flightClass;
+        }
+
+    }
+
