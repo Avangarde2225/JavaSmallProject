412df077c699180d523f45a3258626f10697175a
Serdar <serdar2225@gmail.com>
2020-05-02 23:22:17 -0400

    Abstract

---
+++ src/test/java/ArraysExe.java
@@ -0,0 +1,12 @@
+public class ArraysExe {
+    public static void main(String[] args) {
+        int [] numbers = {2,3,4,5,6};
+        int sum = 0;
+
+        for(int n: numbers){
+         sum +=n;
+
+        }
+        System.out.println(sum);
+    }
+}
