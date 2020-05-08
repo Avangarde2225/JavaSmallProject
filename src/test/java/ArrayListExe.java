905261885161cd9cffdde9bb1cb673a2bf17ce62
Serdar <serdar2225@gmail.com>
2020-05-08 17:20:34 -0400

    Arraylist instantiated

---
+++ src/test/java/ArrayListExe.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class ArrayListExe {
+    private ArrayList<String> ingredients;
+
+    public static void main(String[] args) {
+        ArrayListExe food = new ArrayListExe();
+
+
+        food.ingredients = new ArrayList<>();
+
+        food.ingredients.add("onion");
+        food.ingredients.add("butter");
+        food.ingredients.add("oil");
+        food.ingredients.add("meet");
+
+        System.out.println(food.ingredients);
+    }
+}
