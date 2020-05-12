d0b57d29374f88b6fb6628de09fa6c09c99ef1f0
Serdar <serdar2225@gmail.com>
2020-05-11 22:46:46 -0400

    main changed a bit

--- src/test/java/Inheritance/Inherited.java
+++ src/test/java/Inheritance/Inherited.java
@@ -5,7 +5,11 @@ public class Inherited {
         SingleHouse sh = new SingleHouse();
         System.out.println(sh.toString());
 
-        Warehouse wh = new Warehouse();
-        System.out.println(wh.employees);
+        Warehouse wh = new Warehouse(5, 20000.00);
+
+
+        House h = new Warehouse(2, 3000.00);
+        //System.out.println(h.toString());
+
     }
 }
--- src/test/java/Inheritance/Warehouse.java
+++ src/test/java/Inheritance/Warehouse.java
@@ -3,9 +3,11 @@ package Inheritance;
 public class Warehouse extends House {
     public int employees;
 
-    public Warehouse(){
+    public Warehouse(int room, double price){
         super(3,30000.00);
     }
+
+
 
     public void Sell(){
         if(isNew()){
